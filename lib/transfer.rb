ed6ea4a08fe62abc1f9faa0dc2cf1cf288a10288
Kathleen Howard <kmaryhoward@gmail.com>
2019-05-16 20:24:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
-    else
+    elsif !valid?
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
 
