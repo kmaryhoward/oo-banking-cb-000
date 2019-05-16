319fbcf696de9ab0670edf8b2e1c0b8743679950
Kathleen Howard <kmaryhoward@gmail.com>
2019-05-16 19:56:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? && self.status != "complete"
+    if valid? && sender.balance > amount && self.status != "complete"
       sender.balance -= amount
       receiver.deposit(amount)
       self.status = "complete"
